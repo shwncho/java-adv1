c73838daafe30c63e97f77b75d839e73e1ef8f90
shwncho <shwncho4@gmail.com>
2025-04-27 17:23:25 +0900

    sleep 유틸 생성

---
+++ src/thread/control/CheckedExceptionMain.java
@@ -0,0 +1,20 @@
+package thread.control;
+
+import thread.util.ThreadUtils;
+
+import static thread.util.ThreadUtils.*;
+
+public class CheckedExceptionMain {
+
+    public static void main(String[] args) throws Exception {
+        throw new Exception();
+    }
+
+    static class CheckedRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            sleep(1000);
+        }
+    }
+}
